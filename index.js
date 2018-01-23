acc58fce01d76ee0b1502fde70b68a6b0895801c
Unknown <bastien.barbero@ynov.com>
2018-01-23 12:17:25 +0100

    NodeBot

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const Discord = require('discord.js')
+const webshot = require('webshot');
+
+
 const bot = new Discord.Client()
+
+const { token } = require("./token.json");
+
 
 bot.on('ready', function (){
     bot.user.setActivity('Diablo III').catch(console.error)
@@ -9,6 +15,35 @@ bot.on('message', function(message){
     if (message.content === 'Just do it'){
         message.reply('Dont let your dreams be dreams !')
     }
+    if (message.content === 'i'){
+
+		webshot('google.com', 'google.png', function(err) {
+		});
+
+		message.channel.send({embed: {
+		    color: 3447003,
+		    author: {
+		      name: bot.user.username,
+		      icon_url: bot.user.avatarURL
+		    },
+		    title: "Titre d'un embed",
+		    url: "http://google.com",
+		    description: "Petit test desc.",
+		    image: {
+		    	url : "https://www.w3schools.com/howto/img_fjords.jpg"
+		    },
+		    timestamp: new Date(),
+		    footer: {
+		      icon_url: bot.user.avatarURL,
+		      text: "© Example"
+		    }
+		  }
+		});
+    }
 })
 
-bot.login('NDAyNzc4NTI0NTEzMjA2Mjcy.DT9udQ.6y_XsPf9Awc_SbF5JYOyoJ0vQ9M')
+bot.login(token)
+
+//DOC api swagger
+
+//
